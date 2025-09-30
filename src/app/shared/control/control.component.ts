9b48095049e2a7f10a3dffcf5ae24de841772224
DzinaKor <dinakor1922@gmail.com>
2025-09-30 19:28:57 +0400

    chor: remove extra wrappers in templates for simplify template code

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None // with using of this one folded stules become global and could work properly in this component part
+  encapsulation: ViewEncapsulation.None, // with using of this one folded stules become global and could work properly in this component part
+  host: {
+    class: 'control' // adding class to the host component in template
+  }
 })
 export class ControlComponent {
 label = input.required<string>()
